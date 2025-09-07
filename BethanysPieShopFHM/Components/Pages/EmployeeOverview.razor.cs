07a5055db4e7898d5c1b8d44247e9109df73703c
Brady Kelly <brady_kelly@tutamail.com>
2025-09-07 16:53:34 +0200

    New project

---
+++ BethanysPieShopFHM/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,15 @@
+using BethanysPieShopFHM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopFHM.Components.Pages;
+
+public partial class EmployeeOverview
+{
+    public List<Employee> Employees { get; set; } = new List<Employee>();
+
+    protected override Task OnInitializedAsync()
+    {
+        Employees = MockDataService.Employees;
+        return base.OnInitializedAsync();
+    }
+}
---
+++ BethanysPieShopFHM/Services/MockDataService.cs
@@ -0,0 +1,88 @@
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopFHM.Services;
+
+public class MockDataService
+{
+    private static List<Employee> _employees = [];
+    private static List<JobCategory> _jobCategories = [];
+    private static List<Country> _countries = [];
+
+    public static List<Employee> Employees
+    {
+        get
+        {
+            _countries ??= InitializeMockCountries();
+            _jobCategories ??= InitializeMockJobCategories();
+            _employees ??= InitializeMockEmployees();
+            
+            return _employees;
+        }
+    }
+
+    private static List<Country> InitializeMockCountries()
+    {
+        return [];
+    }
+
+    private static List<JobCategory> InitializeMockJobCategories()
+    {
+        return [];
+    }
+
+    private static List<Employee> InitializeMockEmployees()
+    {
+        var e2 = new Employee
+        {
+            MaritalStatus = MaritalStatus.Married,
+            BirthDate = new DateTime(1980, 1, 16),
+            City = "Pretoria",
+            Email = "mary@bethanyspieshop.com",
+            EmployeeId = 2,
+            FirstName = "Mary",
+            LastName = String.Empty,
+            Gender = Gender.Female,
+            PhoneNumber = "33999909923",
+            Smoker = false,
+            Street = "New Street",
+            Zip = "2000",
+            JobCategory = _jobCategories[1],
+            JobCategoryId = _jobCategories[1].JobCategoryId,
+            Comment = "Lorem Ipsum",
+            ExitDate = null,
+            JoinedDate = new DateTime(2017, 12, 24),
+            Country = _countries[1],
+            CountryId = _countries[1].CountryId
+        };
+        
+        var e3 = new Employee
+        {
+            MaritalStatus = MaritalStatus.Married,
+            BirthDate = new DateTime(1979, 1, 16),
+            City = "Antwerp",
+            Email = "gill@bethanyspieshop.com",
+            EmployeeId = 2,
+            FirstName = "Jane",
+            LastName = String.Empty,
+            Gender = Gender.Female,
+            PhoneNumber = "33999909923",
+            Smoker = false,
+            Street = "New Street",
+            Zip = "2000",
+            JobCategory = _jobCategories[1],
+            JobCategoryId = _jobCategories[1].JobCategoryId,
+            Comment = "Lorem Ipsum",
+            ExitDate = null,
+            JoinedDate = new DateTime(2017, 12, 24),
+            Country = _countries[1],
+            CountryId = _countries[1].CountryId
+        };
+
+        return new List<Employee>() { e2, e3 };        
+    }
+    
+    public void GetEmployee()
+    {
+
+    }
+}
